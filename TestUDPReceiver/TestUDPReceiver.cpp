91bd334b16e8082369435c1965cc6012dc4c553d
Nastia <nlvmr3@gmail.com>
2017-12-09 12:56:57 +0200

    TestUDPSender & TestUDPReciever work together.

---
+++ TestUDPReceiver/TestUDPReceiver.cpp
@@ -0,0 +1,43 @@
+#include <Windows.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+#pragma comment(lib,"ws2_32") 
+
+
+int main(void)
+{
+	WSADATA wsa;
+
+	WSAStartup(MAKEWORD(2, 2), &wsa);
+	SOCKET socketHandle = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+	sockaddr_in localAddress;
+
+	localAddress.sin_family = AF_INET;
+	localAddress.sin_addr.s_addr = inet_addr("10.0.0.60");
+	localAddress.sin_port = htons(3038);
+
+	bind(socketHandle, (SOCKADDR *)(&localAddress), sizeof(localAddress));
+
+	sockaddr_in remoteAddress;
+
+	remoteAddress.sin_family = AF_INET;
+	int remoteAddressSize = sizeof(remoteAddress);
+
+	while (true)
+	{
+		std::string message(1024, 0);
+
+		int count = recvfrom(socketHandle, &message[0], 1024, 0, (sockaddr *)&remoteAddress, &remoteAddressSize);
+
+		std::cout << message.substr(0, count) << " from "<< inet_ntoa(remoteAddress.sin_addr)<<endl;
+	}
+
+	closesocket(socketHandle);
+	WSACleanup();
+
+	return 0;
+}
