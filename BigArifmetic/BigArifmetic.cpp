5700072ae3b4ec7599be07f54bf4c567b4b93ba2
Nastia <nlvmr3@gmail.com>
2017-12-10 13:07:16 +0200

    big numbers multiplicatoin works.

--- BigArifmetic/BigArifmetic.cpp
+++ BigArifmetic/BigArifmetic.cpp
@@ -15,3 +15,47 @@ BigNumber operator +(const BigNumber _number1, const BigNumber _number2)
 	if (transfer) result.digits_[BigNumber::numberSize] = (uint32_t)transfer;
 	return result;
 }
+
+BigNumber smallMultiply(const BigNumber _number, const uint32_t _small)
+{
+	BigNumber result;
+
+	uint64_t transfer = 0;
+
+	for (size_t i = 0; i < BigNumber::numberSize; i++)
+	{
+		const uint64_t number = (uint64_t)_number.digits_[i] * _small + transfer;
+		result.digits_[i] = number & 0x00000000ffffffff;
+		transfer = (number & 0xffffffff00000000) >> 32;
+	}
+
+	if (transfer) result.digits_[BigNumber::numberSize] = (uint32_t)transfer;
+
+	return result;
+}
+
+BigNumber shiftMultiply(const BigNumber _number, const size_t _basePower)
+{
+	BigNumber result;
+
+	for (int i = BigNumber::numberSize - 1 - _basePower; i >= 0; i--)
+	{
+		result.digits_[i + _basePower] = _number.digits_[i];
+	}
+
+	return result;
+}
+
+BigNumber operator *(const BigNumber _number1, const BigNumber _number2)
+{
+	BigNumber result;
+
+	for (size_t i = 0; i < BigNumber::numberSize; i++)
+	{
+		const BigNumber tempNumber1 = smallMultiply(_number1, _number2.digits_[i]);
+		const BigNumber tempNumber2 = shiftMultiply(tempNumber1, i);
+		result = result + tempNumber2;
+	}
+
+	return result;
+}
